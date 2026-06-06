4d2c173b580fc1c37508fa62d1739d6ab1dfa3bd
KnowledgePending <bryanfloodmain@gmail.com>
2026-06-06 17:37:26 +0100

    feat: finish impl

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,7 +1,14 @@
 #include <syslog.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 
 // View syslog output using `journalctl -t writer`
 int main(int argc, char *argv[]){
+    int fd;
+    ssize_t bytes_written;
+    size_t len;
+    
     openlog("writer", LOG_PID, LOG_USER);
     if (argc != 3) {
         syslog(LOG_ERR,
@@ -18,6 +25,28 @@ int main(int argc, char *argv[]){
            "Writing \"%s\" to \"%s\"",
            write_file,
            write_str);
+    fd = open(write_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd == -1) {
+        syslog(LOG_ERR,
+               "Failed to open file %s",
+               write_file);
+        closelog();
+        return 1;
+    }
+
+    len = strlen(write_str);
+
+    bytes_written = write(fd, write_str, len);
+    if (bytes_written != (ssize_t)len) {
+        syslog(LOG_ERR,
+               "Failed to write to file %s",
+               write_file);
+        close(fd);
+        closelog();
+        return 1;
+    }
+
+    close(fd);
     closelog();
 
     return 0;
